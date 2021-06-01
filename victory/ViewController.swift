65ba6e305ab962adc2902ca8fda2d627acfa018b
Nicholas <nichz123@gmail.com>
2021-06-01 09:45:04 +0700

    Initial Commit

---
+++ victory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  victory
+//
+//  Created by Nicholas on 01/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
