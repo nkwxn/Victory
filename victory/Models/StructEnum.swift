afd5ff6c600eebeb98802b39c942ec5e20a80eed
Nicholas <nichz123@gmail.com>
2021-06-03 14:52:11 +0700

    Update Storyboard file to create Main View Controller. Also, start creating struct and enum for Data Model

--- victory/Models/StructEnum.swift
+++ victory/Models/StructEnum.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import UIKit
+
+enum Kelas: Int {
+    case k10 = 10
+    case k11 = 11
+    case k12 = 12
+}
+
+enum Mapel: String {
+    case fisika = "Fisika"
+    case kimia = "Kimia"
+    case biologi = "Biologi"
+    case matematika = "Matematika"
+    
+    func getImage() -> UIImage {
+        switch self {
+        case .fisika:
+            return UIImage(named: "thermometer.snowflake")!
+        case .kimia:
+            return UIImage(named: "drop.triangle")!
+        case .biologi:
+            return UIImage(named: "ladybug")!
+        case .matematika:
+            return UIImage(named: "function")!
+        default:
+            return UIImage()
+        }
+    }
+}
+
+struct Praktikum {
+    var nama: String
+    var gambar: UIImage
+    var kelas: Kelas
+    
+}
