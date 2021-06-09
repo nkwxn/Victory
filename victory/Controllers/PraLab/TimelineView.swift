b0c932ff76bb3609104dfd56db3e7f7731b64d89
imwurie <wuriandita.7@gmail.com>
2021-06-10 00:49:40 +0700

    Add commonInit function

--- victory/Controllers/PraLab/TimelineView.swift
+++ victory/Controllers/PraLab/TimelineView.swift
@@ -27,4 +27,23 @@ class TimelineView: UIView {
     @IBOutlet weak var separatorTiga: UIView!
     @IBOutlet weak var separatorEmpat: UIView!
     
+    @IBOutlet weak var contentView: UIView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        Bundle.main.loadNibNamed("TimelineView", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleWidth]
+    }
+    
 }
