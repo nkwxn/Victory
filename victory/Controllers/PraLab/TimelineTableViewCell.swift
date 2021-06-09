a80f1f06163b530eee85f115a30b5a51859d6312
imwurie <wuriandita.7@gmail.com>
2021-06-09 20:33:31 +0700

    Add TimelineTableViewCell

---
+++ victory/Controllers/PraLab/TimelineTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TimelineTableViewCell.swift
+//  victory
+//
+//  Created by Wuri Dita on 09/06/21.
+//
+
+import UIKit
+
+class TimelineTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
