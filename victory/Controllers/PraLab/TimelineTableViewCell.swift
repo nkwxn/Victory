454941fc6a8790e4fc9de443ea57a4d7123813e6
imwurie <wuriandita.7@gmail.com>
2021-06-09 23:27:12 +0700

    Connect XIB and TableViewCell for Timeline

--- victory/Controllers/PraLab/TimelineTableViewCell.swift
+++ victory/Controllers/PraLab/TimelineTableViewCell.swift
@@ -8,6 +8,24 @@
 import UIKit
 
 class TimelineTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var materiBtn: UIButton!
+    @IBOutlet weak var labSatuBtn: UIButton!
+    @IBOutlet weak var labDuaBtn: UIButton!
+    @IBOutlet weak var labTigaBtn: UIButton!
+    @IBOutlet weak var kuisBtn: UIButton!
+    
+    @IBOutlet weak var materiPill: UIView!
+    @IBOutlet weak var labSatuPill: UIView!
+    @IBOutlet weak var labDuaPill: UIView!
+    @IBOutlet weak var labTigaPill: UIView!
+    @IBOutlet weak var kuisPill: UIView!
+    
+    @IBOutlet weak var labBg: UIView!
+    @IBOutlet weak var separatorSatu: UIView!
+    @IBOutlet weak var separatorDua: UIView!
+    @IBOutlet weak var separatorTiga: UIView!
+    @IBOutlet weak var separatorEmpat: UIView!
 
     override func awakeFromNib() {
         super.awakeFromNib()
