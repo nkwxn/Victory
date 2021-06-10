2dec01e225a789a4cfc56087d4705fb7c5fc698d
Yuliana <yuliana.aulele@gmail.com>
2021-06-10 14:03:40 +0900

    commit before pull

---
+++ victory/Controllers/Paket - Kuis/OnboardingView.swift
@@ -0,0 +1,22 @@
+//
+//  OnboardingView.swift
+//  victory
+//
+//  Created by Yuliana Aulele on 10/06/21.
+//
+
+import UIKit
+
+class OnboardingView: UIView {
+
+    @IBAction func onButtonPressed(_ sender: Any) {
+    }
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
