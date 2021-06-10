607765182ee742a3506d05a4cf1337c3a7fb2752
Yuliana <yuliana.aulele@gmail.com>
2021-06-10 17:32:18 +0900

    Add skor kuis

---
+++ victory/Views/Paket - Kuis/SkorView.swift
@@ -0,0 +1,20 @@
+//
+//  SkorView.swift
+//  victory
+//
+//  Created by Yuliana Aulele on 10/06/21.
+//
+
+import UIKit
+
+class SkorView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
