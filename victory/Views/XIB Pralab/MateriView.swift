ca2cd08d47a01f008fbbf4339f506ec24aa5168b
imwurie <wuriandita.7@gmail.com>
2021-06-11 15:51:18 +0700

    Add materiView xib anad swift file

---
+++ victory/Views/XIB Pralab/MateriView.swift
@@ -0,0 +1,20 @@
+//
+//  MateriView.swift
+//  victory
+//
+//  Created by Wuri Dita on 11/06/21.
+//
+
+import UIKit
+
+class MateriView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
