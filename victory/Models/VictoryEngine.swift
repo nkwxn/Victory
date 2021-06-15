6a70a2424a4811b9c47ea5134b2cbd2c28d1263a
oddesa <73622874+oddesa@users.noreply.github.com>
2021-06-15 08:10:58 +0700

    Update VictoryEngine (minus height configuration)

--- victory/Models/VictoryEngine.swift
+++ victory/Models/VictoryEngine.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SpriteKit
 
 class VictoryEngine{
     /// set up ketinggian masih nunggu pengaturan boundary dari gamescene
@@ -13,7 +14,10 @@ class VictoryEngine{
     //for freestyle mode
     var kecAwal : Float = 100
     var sudutAwal : Double = 45
-    var gravitasi : Float = 9.8
+    var gravitasiEngine = Float(SKPhysicsWorld().gravity.dy * 150)
+    var waktu : Double = 1
+    var gravitasiReal : Float = 9.8
+    var poinKeMeter : Float = 50
     
     var dx: CGFloat {
         get{
@@ -27,36 +31,73 @@ class VictoryEngine{
         }
     }
     
-    var ketinggianMax : Float {
+    var ketinggianMaxReal : Float {
         get {
-            return (pow(kecAwal, 2) * pow(Float(__sinpi(sudutAwal/180)), 2))/(2*gravitasi)
-        }
-    }
-    var jarakMax : Float {
-        get {
-            return (pow(kecAwal,2)/gravitasi) * Float(__sinpi((2*sudutAwal)/180))
+            return (pow(kecAwal, 2) * pow(Float(__sinpi(sudutAwal/180)), 2))/(2*gravitasiReal)
         }
     }
     
-    var waktuKetinggianMax: Float {
+    var ketinggianMaxEngine : Float {
         get {
-            return kecAwal * Float(__sinpi(sudutAwal/180)) / gravitasi
+            return (pow(kecAwal, 2) * pow(Float(__sinpi(sudutAwal/180)), 2))/(2*gravitasiEngine)
+        }
+    }
+    var jarakMaxEngine : Float {
+        get {
+            return (pow(kecAwal,2)/gravitasiEngine) * Float(__sinpi((2*sudutAwal)/180))
         }
     }
     
-    var waktuJarakMax: Float {
+    var jarakMaxReal : Float {
         get {
-            return 2 * waktuKetinggianMax
+            return (pow(kecAwal,2)/gravitasiReal) * Float(__sinpi((2*sudutAwal)/180))
         }
     }
+    
+    var waktuKetinggianMaxEngine: Float {
+        get {
+            return kecAwal * Float(__sinpi(sudutAwal/180)) / gravitasiEngine
+        }
+    }
+    
+    var waktuKetinggianMaxReal: Float {
+        get {
+            return kecAwal * Float(__sinpi(sudutAwal/180)) / gravitasiReal
+        }
+    }
+    
+    var waktuJarakMaxEngine: Float {
+        get {
+            return 2 * waktuKetinggianMaxEngine
+        }
+    }
+    
+    var waktuJarakMaxReal: Float {
+        get {
+            return 2 * waktuKetinggianMaxReal
+        }
+    }
+
     
     //callable function
-    func kecepatanXDariSudut(sudutTembak: Double, kecepatanAwal: Float) -> CGFloat {
+    func kecepatanXAwalEngine(sudutTembak: Double, kecepatanAwal: Float) -> CGFloat {
+        return CGFloat(kecepatanAwal * poinKeMeter * Float(__cospi(sudutTembak/180)))
+    }
+    
+    func kecepatanXAwalReal(sudutTembak: Double, kecepatanAwal: Float) -> CGFloat {
         return CGFloat(kecepatanAwal * Float(__cospi(sudutTembak/180)))
     }
     
-    func kecepatanYDariSudut(sudutTembak: Double, kecepatanAwal: Float) -> CGFloat {
+    func kecepatanYAwalEngine(sudutTembak: Double, kecepatanAwal: Float) -> CGFloat {
+        return CGFloat(kecepatanAwal * poinKeMeter * Float(__sinpi(sudutTembak/180)))
+    }
+    
+    func kecepatanYAwalReal(sudutTembak: Double, kecepatanAwal: Float) -> CGFloat {
         return CGFloat(kecepatanAwal * Float(__sinpi(sudutTembak/180)))
+    }
+    
+    func kecepatanX(sudutTembak: Double, kecepatanAwal: Float) -> CGFloat {
+        return CGFloat(kecepatanAwal * Float(__cospi(sudutTembak/180)))
     }
     
     func sudutDariKecepatanAwalY(kecepatanAwal: Float, kecepatanAwalY: Float) -> Float {
@@ -67,24 +108,55 @@ class VictoryEngine{
         return acos(kecepatanAwalX/kecepatanAwal)*180/Float.pi
     }
     
-    func titikTertinggiDariKecepatanAwal(kecepatanAwal: Float, sudutTembak: Double, gravitasi: Float) -> Float {
+    func titikTertinggiDariKecepatanAwalReal(kecepatanAwal: Float, sudutTembak: Double, gravitasi: Float) -> Float {
         return (pow(kecepatanAwal, 2) * pow(Float(__sinpi(sudutTembak/180)), 2))/(2*gravitasi)
+        
     }
     
-    func titikTertinggiDariKecepatanAwalY(kecepatanAwalY: Float, gravitasi: Float) -> Float {
+    func titikTertinggiDariKecepatanAwalYReal(kecepatanAwalY: Float, gravitasi: Float) -> Float {
         return (pow(kecepatanAwalY, 2))/(2*gravitasi)
     }
     
-    func jarakTerjauhDariKecepatanAwal(kecepatanAwal: Float, sudutTembak: Double, gravitasi: Float) -> Float{
+    func jarakTerjauhDariKecepatanAwalReal(kecepatanAwal: Float, sudutTembak: Double, gravitasi: Float) -> Float{
         return (pow(kecepatanAwal,2)/gravitasi) * Float(__sinpi((2*sudutTembak)/180))
     }
     
-    func waktuUntukTitikTertinggi(kecepatanAwal: Float, sudutTembak:Double, gravitasi: Float) -> Float{
+    func waktuUntukTitikTertinggiReal(kecepatanAwal: Float, sudutTembak:Double, gravitasi: Float) -> Float{
         return kecepatanAwal * Float(__sinpi(sudutTembak/180)) / gravitasi
     }
     
-    func waktuUntukJarakTerjauh(kecepatanAwal: Float, sudutTembak:Double, gravitasi: Float) -> Float{
-        return 2 * kecepatanAwal * Float(__sinpi(sudutTembak/180)) / gravitasi
+    func waktuUntukJarakTerjauhEngine(kecepatanAwal: Float, sudutTembak:Double, gravitasi: Float) -> Float{
+        return 2 * poinKeMeter * kecepatanAwal * Float(__sinpi(sudutTembak/180)) / (-gravitasiEngine)
     }
+    
+    func waktuUntukJarakTerjauhReal(kecepatanAwal: Float, sudutTembak:Double, gravitasi: Float) -> Float{
+        return 2 * kecepatanAwal * Float(__sinpi(sudutTembak/180)) / (gravitasi)
+    }
+    
+    
+    // MARK: - FUNGSI TERHADAP WAKTU
+    func xProyektilTerhadapWaktuEngine(kecepatanAwal: Float, sudutTembak: Double, waktu: Float) -> CGFloat {
+        return CGFloat(Double(kecepatanAwal * poinKeMeter) * __cospi(sudutTembak/180) * Double(waktu) )
+    }
+    
+    func xProyektilTerhadapWaktuReal(kecepatanAwal: Float, sudutTembak: Double, waktu: Float) -> CGFloat {
+        return CGFloat(Double(kecepatanAwal) * __cospi(sudutTembak/180) * Double(waktu) )
+    }
+    
+    func yProyektilTerhadapWaktuEngine(kecepatanAwal: Float, sudutTembak: Double, waktu: Float, gravitasi: Float)-> CGFloat {
+        let part1 = (kecepatanAwal * poinKeMeter * Float(__sinpi(sudutTembak/180) * Double(waktu)))
+        let part2 = (0.5 * pow(Float(waktu),2) * gravitasiEngine)
+        return CGFloat(part1 + part2)
+    }
+    
+    func yProyektilTerhadapWaktuReal(kecepatanAwal: Float, sudutTembak: Double, waktu: Float, gravitasi: Float)-> CGFloat {
+        let part1 = (kecepatanAwal * Float(__sinpi(sudutTembak/180) * Double(waktu)))
+        let part2 = (0.5 * pow(Float(waktu),2) * gravitasi)
+        return CGFloat(part1 - part2)
+    }
+    
+//    func kecepatanY(sudutTembak: Double, kecepatanAwal: Float, waktu: Float, gravitasi: Float) -> CGFloat {
+//        return CGFloat((kecepatanAwal * Float(__sinpi(sudutTembak/180))) - (gravitasi * waktu))
+//    }
 }
 
